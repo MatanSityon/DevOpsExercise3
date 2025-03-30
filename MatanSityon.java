47ebd4b833ec4c364f7c6ebaa480a75892637f30
MatanSityon <matansit04@gmail.com>
2025-03-30 13:05:20 +0300

    Changed my name to a different statement

--- MatanSityon.java
+++ MatanSityon.java
@@ -1,7 +1,7 @@
 public class MatanSityon{
 public static void main(String[] args) {
 for (int i=1;i<=10; i++) {
-System.out.println("Matan Sityon ..."+i);
+System.out.println("My name is gone ..."+i);
 }
 }
 }
